de558bce53292431ed5dce109c517daf9e8d9bdd
zermaineeee <60999170+zermaineeee@users.noreply.github.com>
2020-06-16 22:25:35 +0800

    Updated front end

--- Frontend/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./LoginStyle.scss";
-import { LoginPage } from "./Loginpage";
-import { BrowserRouter } from "react-router-dom";
-
-ReactDOM.render(<BrowserRouter><LoginPage /></BrowserRouter>, document.getElementById("root"));
-
